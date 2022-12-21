4531eea235fa453afdc88dd2816f01fbf83f01d7
VikingForcee <119648800+VikingForcee@users.noreply.github.com>
2022-12-21 21:16:14 +0530

    commit

---
+++ Assignment 2.py
@@ -0,0 +1,68 @@
+import time
+print("Assignment-2")
+"""
+print()
+a = "Python is a case sensitive language"
+print(a[::-1])
+
+str = "a case sensitive"
+print(str)
+index = 0
+new_charector= "Object oriented"
+str = new_charector
+print(str)
+
+print()
+Enter_text= input()
+result = Enter_text.find('a')
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+print()
+print()
+name = "Vibhor"
+SID = 22104014
+Department = "Electrical"
+CGPA = 10
+print("Hey, ", name, "Here!")
+print("My SID is ", SID)
+print("I am from ",Department," department and my CGPA is", CGPA)
+time.sleep(2)
+
+print()
+
+print()
+a = 56
+b=10
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(a << 2)
+print(b << 2)
+print(a >> 2)
+print(b >> 4)
+
+print()
+print()
+time.sleep(2)
+print("Enter_3_numbers = ")
+a1 = input("A1 = ")
+a2= input("A2 = ")
+a3 = input("A3 = ")
+print("Maximum of given 3 is")
+print(max(int(a1),int(a2),int(a3)))
+time.sleep(10)
+
+
